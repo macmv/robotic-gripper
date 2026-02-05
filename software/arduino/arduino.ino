00c256545ec364eac977ca4de0e8d4a56b12a2d8
Neil Macneale V <neil.macneale.v@gmail.com>
2026-02-04 17:24:20 -0800

    Toggle the LED from serial commands

--- software/arduino/arduino.ino
+++ software/arduino/arduino.ino
@@ -1,10 +1,16 @@
 void setup() {
+  // This is an even divisor of F_CPU (16000000), so we get no error
+  Serial.begin(500000);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  if (Serial.available() > 0) {
+    int byte = Serial.read();
+    if (byte == 'y') {
+      digitalWrite(13, HIGH);
+    } else if (byte == 'n') {
+      digitalWrite(13, LOW);
+    }
+  }
 }
